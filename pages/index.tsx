f55a645a5a2d45382f0a7504cbfd017d938600af
HCarrer <hmcarrer@gmail.com>
2022-12-26 00:22:36 -0300

    fixes CLIENT_ID env var name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const CLIENT_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 
 export default function Home() {
 
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <div className="flex justify-center items-center w-full h-screen">
         <div className="w-fit border border-black">
-          <Link href={`http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read_all`}>Fazer Login</Link>
+          <Link href={`http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=activity:read_all`}>Fazer Login</Link>
         </div>
       </div>
     </>
