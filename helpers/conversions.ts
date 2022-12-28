668d46212851706ca5d51b0be5d50742cd4c6bda
HCarrer <hmcarrer@gmail.com>
2022-12-27 21:51:05 -0300

    adds conversion helpers

---
+++ helpers/conversions.ts
@@ -0,0 +1,17 @@
+export const to_kph = (mps_speed: string) => {
+  return (parseFloat(mps_speed) * 3.6).toFixed(2)
+}
+
+export const to_km = (meters: string) => {
+  return (parseFloat(meters) / 1000).toFixed(2)
+}
+
+export const format_date = (date: string) => {
+  const separated_date = date.split(/[-T:Z]+/)
+  const year = separated_date[0]
+  const month = separated_date[1]
+  const day = separated_date[2]
+  const hour = separated_date[3]
+  const minute = separated_date[4]
+  return `${day}/${month}/${year} às ${hour}:${minute}`
+}
