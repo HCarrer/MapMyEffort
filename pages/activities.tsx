d0f3de118df74bef3bbd9eb893c19bc6a61f0998
HCarrer <hmcarrer@gmail.com>
2023-01-01 18:13:11 -0300

    cleans up activities page using activityContext

--- pages/activities.tsx
+++ pages/activities.tsx
@@ -1,38 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
-import { useCode } from "../context/authCodeContext";
-import { listActivities } from "../instances/strava";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useActivity } from "../context/activityContext";
+import { format_date, to_km, to_kph } from "../helpers/conversions";
 
 export default function Home() {
-  const { refreshToken } = useCode()
-  const [activities, setActivities] = useState([])
-
-  const listAllActivities = async () => {
-    console.log(refreshToken)
-    const { data } = await listActivities(refreshToken)
-    setActivities(data.data)
-    return data.data
-  }
+  const { activities, setAllActivities } = useActivity()
 
   useEffect(() => {
-    listAllActivities()
+    setAllActivities()
   }, [])
-
   
   return (
     <>
       <div className="w-full">
         <span>activities</span>
         <div className="flex gap-x-10 gap-y-10 flex-wrap">
-          {activities.map((activity) => {
+          {!!activities && activities.map((activity) => {
             return (
               <>
-                <div key={activity['id']} className="flex flex-col gap-y-4 w-fit h-80 border border-black">
-                  <span>distance: {activity['distance']}</span>
-                  <span>total elevation gain: {activity['total_elevation_gain']}</span>
-                  <span>average speed: {activity['average_speed']}</span>
-                  <span>start date: {activity['start_date']}</span>
-                </div>
+                <Link key={activity.id} className="flex flex-col gap-y-4 w-fit h-80 border border-black" href={`/activities/${activity.id}`}>
+                  <span>distance: {to_km(activity.distance)} km/h</span>
+                  <span>total elevation gain: {activity.total_elevation_gain} m</span>
+                  <span>average speed: {to_kph(activity.average_speed)} km/h</span>
+                  <span>start date: {format_date(activity.start_date)}</span>
+                </Link>
               </>
             )
           })}
